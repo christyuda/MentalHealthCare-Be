2cd7b4f5d00a94cabbce5c94f52d2c6ecce44358
christyuda <chrisyuda@ulbi.ac.id>
2023-09-24 20:24:07 +0700

    push beranda

--- app.js
+++ app.js
@@ -88,8 +88,35 @@ app.use('/api/statistics', statisticsRoutes);
 
 // Rute beranda
 app.get('/', (req, res) => {
-  res.send('Selamat datang di API Mental Health Care!');
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Selamat datang di API Mental Health Care</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f0f0f0;
+          text-align: center;
+          padding: 50px;
+        }
+        h1 {
+          color: #333;
+        }
+        p {
+          color: #666;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Selamat datang di API Mental Health Care!</h1>
+      <p>Ini adalah backend untuk aplikasi layanan kesehatan mental.</p>
+    </body>
+    </html>
+  `);
 });
+
 
 // Menjalankan server
 app.listen(port, () => {
